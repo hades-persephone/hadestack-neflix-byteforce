a9ee9f63b67f23e26465a85146f37fa8072b6760
sukuna <phamvanhiep.work@gmail.com>
2025-05-10 17:23:02 +0700

    feat(movie_service) update missing file sync-service

---
+++ service/sync-service/src/test/java/io/watch/sync/SyncServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.watch.sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
