66dc6ec81786ec3db39b13669e4e1d63608c2e79
sukuna <phamvanhiep.work@gmail.com>
2025-05-12 16:49:45 +0700

    feat(history_service) update code missing file

---
+++ service/history-service/src/main/java/io/watch/history/HistoryServiceApplication.java
@@ -0,0 +1,15 @@
+package io.watch.history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class HistoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HistoryServiceApplication.class, args);
+    }
+
+}
